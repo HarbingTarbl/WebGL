6a2af0ebe24ba9eb16a05839489ee6dd78b24601
Michael Harris <harbingtarbl@gmail.com>
2014-10-08 04:10:07 -0400

    Displacement+Normal

--- crytek-ssao.js
+++ crytek-ssao.js
@@ -20,7 +20,7 @@ var crytekSSAO = function(width, height, near, far) {
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, pass.framebuffer);
     gl.bindTexture(gl.TEXTURE_2D, pass.diffuseTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
